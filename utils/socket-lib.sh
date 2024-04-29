1d26587757211ae53aedac24c47fd0e22eee4923
Orestes Sanchez <orestes.sanchezbenavente@telefonica.com>
2024-04-30 01:12:00 +0200

    Add new commands.

--- utils/socket-lib.sh
+++ utils/socket-lib.sh
@@ -2,12 +2,20 @@
 SERVER=${SERVER:-192.168.1.46}
 PORT=${PORT:-8888}
 
-function open_dome(){
-  (echo -n "(SET:OPEN:0)" && sleep 1) | nc $SERVER $PORT
+function abort_dome(){
+  (echo -n "(SET:ABORT:0)" && sleep 1) | nc $SERVER $PORT
 }
 
 function close_dome(){
   (echo -n "(SET:CLOSE:0)" && sleep 1) | nc $SERVER $PORT
+}
+
+function disable_dome(){
+  (echo -n "(SET:DISABLE:0)" && sleep 1) | nc $SERVER $PORT
+}
+
+function open_dome(){
+  (echo -n "(SET:OPEN:0)" && sleep 1) | nc $SERVER $PORT
 }
 
 function isopen_dome(){
@@ -17,3 +25,4 @@ function isopen_dome(){
 function isclosed_dome(){
   (echo -n "(GET:CLOSED:0)" && sleep 1) | nc $SERVER $PORT
 }
+
