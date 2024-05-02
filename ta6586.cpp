ec21e4747aaa96427d130f9564f7fa6af51d3cd8
Orestes Sanchez <orestes.sanchezbenavente@telefonica.com>
2024-05-02 21:51:48 +0200

    Set builtin led off when stopping motors.

--- ta6586.cpp
+++ ta6586.cpp
@@ -42,11 +42,16 @@ void TA6586::motorOff() {
   DEBUG_INFO("Switch motor off");
   oledConsole->print("Switch motor off");
   // Make sure motors are stopped
-  digitalWrite(FUNC_DIRECTION_A, LOW);   // Set actuator voltage leads to open actuator
-  digitalWrite(FUNC_ACTIVATION_A, LOW);  // Set actuator in motion
 
-  digitalWrite(FUNC_DIRECTION_B, HIGH);   // Set actuator voltage leads to open actuator
-  digitalWrite(FUNC_ACTIVATION_B, HIGH);  // Set actuator in motion
+  // Set both to high to disable motor. 
+  // HIGH means also that LED_BUILTIN will also switch off in case we are using
+  // LED_BUILTIN for the motor.
+  digitalWrite(FUNC_DIRECTION_A, HIGH);   
+  digitalWrite(FUNC_ACTIVATION_A, HIGH);  
+
+  // Set both to high to disable motor.
+  digitalWrite(FUNC_DIRECTION_B, HIGH);
+  digitalWrite(FUNC_ACTIVATION_B, HIGH);
 }
 
 void TA6586::motorOn() {
