d1fc75f0f77350566578a5e4bc1e824edee4cb5e
Orestes Sanchez Benavente <orestes@estotienearreglo.es>
2025-03-17 00:10:09 +0100

    Remove obsolete include

--- drv8871.cpp
+++ drv8871.cpp
@@ -1,7 +1,6 @@
 #ifndef __rolloff_linear_actuator_DRV8871__
 #define __rolloff_linear_actuator_DRV8871__
 
-#include <ESP8266WiFi.h>
 #include "motor.h"
 
 const char* DRV8871_VERSION_ID = "V1.6-esp-wifimanager-magnet-DRV8871";
